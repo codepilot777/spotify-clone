0b6538e232c137e78851174177b37c8c28d86241
Lok Yiu Yeung <ryanyeung@Loks-MacBook-Air.local>
2023-06-01 10:33:38 -0700

    added Supabase Provider and User Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import { Figtree } from 'next/font/google'
 
+import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
@@ -17,9 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   )
