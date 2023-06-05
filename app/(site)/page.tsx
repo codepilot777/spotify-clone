b93b8bc6f59d7e718942b9f53d44142f03e2e1d4
codepilot777 <codepilot777@gmail.com>
2023-06-05 21:33:02 +0800

    fixed getSongs

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,10 @@
 import Header from "@/components/Header"
 import ListItem from "@/components/ListItem"
-import getSongs from '@/actions/getSongs'
+import getSongs from '@/actions/getSongs';
 
 export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
   const songs = await getSongs();
   return (
     <div className="w-full h-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900">
